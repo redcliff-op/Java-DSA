ceadc56b0f1b497b10e1cfa06314a2e61b182d7c
redcliff-op <devyashjangid@gmail.com>
2023-07-21 21:45:58 +0530

    [Recursion]-Calculate power

---
+++ Recursion/Power.java
@@ -0,0 +1,12 @@
+public class Power {
+	static int power(int x, int n) {
+		if(n==0){
+            return 1;
+        }else{
+            return x*power(x, n-1);
+        }
+	}
+    public static void main(String[] args){
+        System.out.println(power(2, 3));
+    }
+}
