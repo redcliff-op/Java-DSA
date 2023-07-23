fa2e8558a5abba1b8550e4fed64a9a6ed6bcb132
redcliff-op <devyashjangid@gmail.com>
2023-07-23 13:30:49 +0530

    [Recursion]-Tower of Hanoi
    Tower of Hanoi is a mathematical puzzle where we have three rods and n disks. The objective of the puzzle is to move all disks from source rod to destination rod using third rod (say auxiliary). The rules are :
    
    1) Only one disk can be moved at a time.
    2) A disk can be moved only if it is on the top of a rod.
    3) No disk can be placed on the top of a smaller disk.
    
    Print the steps required to move n disks from source rod to destination rod.
    
    Source Rod is named as 'a', auxiliary rod as 'b' and destination rod as 'c'.

---
+++ Recursion/Tower_Of_Hanoi.java
@@ -0,0 +1,13 @@
+public class Tower_Of_Hanoi {
+  public static void towerOfHanoi(int disks, char source, char auxiliary, char destination){
+    if(disks==0){
+        return;
+    }else if(disks==1){
+      System.out.println(source+" "+destination);
+      return;
+    }   
+    towerOfHanoi(disks-1,source,destination,auxiliary);
+    System.out.println(source+" "+destination);
+    towerOfHanoi(disks-1,auxiliary,source,destination);
+  }
+}
