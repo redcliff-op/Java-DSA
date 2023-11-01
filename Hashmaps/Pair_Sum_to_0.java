aeba5969720f75ddf5f1a4b4e1008714620bd483
redcliff-op <devyashjangid@gmail.com>
2023-11-01 09:38:40 +0530

    [HashMaps]-Sum Pair to 0
    Given a random integer array A of size N. Find and print the count of pair of elements in the array which sum up to 0.

---
+++ Hashmaps/Pair_Sum_to_0.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class Pair_Sum_to_0 {
+  public static int PairSum(int[] arr, int size) {
+    HashMap<Integer,Integer> check = new HashMap<Integer,Integer>();
+    for(int i=0;i<size;i++){
+      if(check.containsKey(arr[i])){
+        check.put(arr[i], check.get(arr[i])+1);
+      }else{
+        check.put(arr[i],1);
+      }
+    }
+    int count=0;
+    for(int i=0;i<size;i++){
+      if(check.containsKey(-arr[i])){
+        count += check.get(-arr[i]);
+      }
+      if(arr[i]==0){
+        count--;
+      }
+    }
+    return count;
+	}
+}
