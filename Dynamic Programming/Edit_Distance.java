1575167f553699a0eef198a0225246835376ff81
redcliff-op <devyashjangid@gmail.com>
2023-12-02 19:34:16 +0530

    [Dynamic Programming]-Edit Distance Using Recursion
    Given two strings s and t of lengths m and n respectively,
    find the edit distance between the strings.
    Edit Distance
    Edit Distance of two strings is minimum number of
    operations required to make one string equal to other.
    In order to do so you can perform any of the following
    three operations only:
    1. Delete a character
    2. Replace a character with another one
    3. Insert a character

---
+++ Dynamic Programming/Edit_Distance.java
@@ -0,0 +1,18 @@
+public class Edit_Distance {
+  public static int editDistance(String s1, String s2){
+    if(s1.length()==0){
+      return s2.length();
+    }
+    if(s2.length()==0){
+      return s1.length();
+    }
+    if(s1.charAt(0)==s2.charAt(0)){
+      return editDistance(s1.substring(1), s2.substring(1));
+    }else{
+      int op1 = editDistance(s1, s1.charAt(0) + s2);
+      int op2 = editDistance(s1, s2.substring(1));
+      int op3 = editDistance(s1, s1.charAt(0) + s2.substring(1));
+      return 1 + Math.min(op1,Math.min(op2, op3));
+    }
+  }
+}
